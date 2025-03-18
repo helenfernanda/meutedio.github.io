a03d4048860a19887c6cd283ccf77318ae55b352
Helen Fernanda <39865869+helenfernanda@users.noreply.github.com>
2025-03-18 16:29:21 -0300

    testando
    testando

---
+++ head.php
@@ -0,0 +1,9 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PXGKE0RFD4"></script>
+     
+<title>Meu Tédio por Helen Fernanda</title>
+	
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+<link type='text/css' rel='stylesheet' href='estilo.css'/>
+
+<base href="https://meuted.io" />
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		<?php include 'head.php' ?>
+	
+	<style>
+	
+::-webkit-scrollbar {background:#333; width:15px; height:15px;} /* barra de rolagem toda */
+::-webkit-scrollbar-thumb {background:#333; box-shadow:0px 0px 2px 2px #fff inset;} /* parte da barra que se movimenta */
+::-webkit-scrollbar-thumb:hover {background:#333; box-shadow:0px 0px 2px 2px #f90 inset;} /* parte da barra que se movimenta */
+::-webkit-scrollbar-thumb:active {background: #333; box-shadow:0px 0px 2px 2px #9c0 inset;} /* parte da barra que se movimenta */
+/* ::-webkit-scrollbar-button {background:;} /* ponta da barra de rolagem */
+::-webkit-scrollbar-track {background:#333;} /* fundo da barra de rolagem, parte fixa */
+::-webkit-scrollbar-track-piece {background:#333;} /* fundo da barra de rolagem, parte que ainda falta rolar */
+::-webkit-scrollbar-corner {background:#333;} /* canto inferior direito da barra de rolagem */
+
+.gcse-searchresults{background-color:#fff,0;}	
+body {margin:30px; font-family:'trebuchet ms', georgia, serif; font-size:20px;}
+a, a:hover, a:link {color:#ccc; text-decoration:none;}
+a:hover {border-bottom:1px dashed #ccc;}
+</style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+
+	<h1>meuted.io</h1>
+
+	<p>Este é apenas um site acessório do blog <a target="_blank" href="http://www.meutedio.com">www.meutedio.com</a>.</p>
+
+	<div style='position:fixed; right:0; margin-right:0; text-align:right; float:right;'>
+	</div>
+	
+	
+
+<!-- início busca Google -->
+<script async src="https://cse.google.com/cse.js?cx=partner-pub-2810932839099491:jw7n8ai459q">
+</script>
+<div class="gcse-searchbox"></div>
+<div class="gcse-searchresults"></div>
+<!-- fim busca Google -->
+</dl>
+	
+	<h2 id='menu'>Menu</h2>
+	
+	<ul>
+		<li><a href="tag-audio.html">Tag &lt;audio&gt;</a></li>
+		<li><a href="tag-video.html">Tag &lt;video&gt;</a></li>
+		<!-- <li><a href="audio-streampad.html">Tag &lt;audio&gt; + Streampad</a></li> -->
+		<li><a href="mark.html">Tag &lt;mark&gt;</a></li>
+		<li><a href="embed.html">Tags &lt;embed&gt;, &lt;iframe&gt; e &lt;object&gt;</a></li>
+		<li><a href="caixas-com-degrade.html">CSS | Caixas com degradê</a></li>
+		<li><a href="hover.html">CSS | Efeito :hover com transição suave</a></li>
+		<li><a href="scrollbar-colorida.html">CSS | Scrollbar colorida</a></li>
+		<li><a href="linhahorizontal.html">CSS | Linha horizontal</a></li>
+		<li><a href="geranick.html">JavaScript | Gerador de nick anos 2000</a></li>		
+		<!-- <li><a href="streampad.html">Streampad - mp3 player</a></li> -->
+	</ul>
+
+
+	<!-- starts Google Analytics -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-28181690-7', 'auto');
+  ga('send', 'pageview');
+</script> 
+	<!-- ends Google Analytics -->
+</body>	
+
+</html>
